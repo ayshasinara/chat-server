0060d9dbfac69f2a3771d1fd55a08dd0bf01b287
nkaysha <aysha@neokred.tech>
2025-04-19 10:53:05 +0530

    convert markdown from backend end frontend

--- server.js
+++ server.js
@@ -1,31 +1,22 @@
-const { Server } = require('socket.io');
-const io = new Server(3030, {
-  cors: {
-    origin: '*', 
-  }
+const express = require('express');
+const cors = require('cors');
+const { marked } = require('marked');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// server.js or index.js
+app.get('/ping', (req, res) => {
+  
+  res.send('pong');
 });
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
-
-
-  socket.on('joinRoom', (roomId) => {
-    socket.join(roomId); 
-    console.log(`User joined room: ${roomId}`);
-  });
-
-
-  socket.on('markdownUpdate', ({ roomId, markdown }) => {
-
-    socket.to(roomId).emit('markdownUpdate', markdown);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-  });
+app.post('/convert', (req, res) => {
+  const { markdown } = req.body;
+  console.log(markdown,"sdasdas")
+  const html = marked(markdown || '');
+  res.json({ html });
 });
 
-
-io.listen(3030, () => {
-  console.log('Socket.io server running on port 3030');
-});
+app.listen(3001, () => console.log('Markdown server running on port 3001'));
